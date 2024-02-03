af4ce096530400e284dadd4e6a0af5567ae3a6de
JinseoKim <eunoia@handong.ac.kr>
2024-02-03 18:37:06 +0900

    User CRUD Complete

--- src/main/java/org/example/repository/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.repository;
-
-import org.example.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
