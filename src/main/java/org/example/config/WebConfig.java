b591e639ff3e9faa7549e0bec722170323ef43ba
JinseoKim <eunoia@handong.ac.kr>
2024-02-05 14:10:45 +0900

    CORS 해결

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://https://daangn-clone-front.vercel.app"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://daangn-clone-front.vercel.app"));
         config.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "PATCH", "DELETE"));
         config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
         config.setAllowCredentials(true);
